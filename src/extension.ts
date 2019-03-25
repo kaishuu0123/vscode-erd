33db7e91d2497eea4b472637f141903bd7685000
bennetgallein <bennet@intranetproject.net>
2019-03-25 22:01:33 +0100

    fixed small issue which made the extension not working

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { outputPanel } from "./outputPanel";
-import { CommandManager } from "./CommandManager";
+import { CommandManager } from "./commandManager";
 import { ShowPreviewCommand, SaveSvgCommand, SavePdfCommand, SavePngCommand } from "./commands";
 import { PreviewManager } from "./previews/previewManager";
 import { ErdContentProvider } from "./erdContentProvider";
